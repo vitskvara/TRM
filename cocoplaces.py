48a304c56bb6e75e70a272fc8da788987f2e4b48
Vít Škvára <vit.skvara@gmail.com>
2022-06-10 11:34:17 +0200

    cocoplaces

--- cocoplaces.py
+++ cocoplaces.py
@@ -1,88 +1,69 @@
 # EDIT THESE PATHS ACCORDING TO WHERE YOUR RAW DATA IS SAVED/TO BE SAVED
-output_dir = '../sgad_data/places'
-places_dir = '../sgad_data/places/data_256'
+imsize = 64
+cocopath = '/home/skvarvit/generativead-sgad/sgad_data/raw_datasets/coco'
+placespath = '/home/skvarvit/generativead-sgad/sgad_data/raw_datasets/places'
+outpath = '/home/skvarvit/generativead-sgad/sgad_data/raw_datasets/cocoplaces'
 
-import os, subprocess
 import numpy as np
+import h5py, os, sys
 import random
-from PIL import Image
-from skimage.transform import resize
-import matplotlib
-matplotlib.use('Agg')
 
-import h5py
+# if the coco.py and places.py files are not changed, this is the matching of the classes
+# in the uniform files
+# boat/beach = 0/0
+# airplane/bamboo_forest = 1/1
+# truck/canyon = 2/2
+# dog/forest = 3/3
+# zebra/ball_pit = 4/4
+# horse/orchard = 5/5
+# bird/rock_arch = 6/6
+# train/shower = 7/7
+# bus/ski_slope = 8/8
+# motorcycle/wheat_field = 9/9
 
-biased_places = ['b/beach',
-                 'c/canyon',
-                 'b/building_facade',
-                 's/staircase',
-                 'd/desert/sand',
-                 'c/crevasse',
-                 'b/bamboo_forest',
-                 'f/forest/broadleaf',
-                 'b/ball_pit',
-                 ]
+# infiles
+cocotrainf = os.path.join(cocopath, f'processed_{imsize}/train.h5py')
+cocovalf = os.path.join(cocopath, f'processed_{imsize}/validation.h5py')
+placesf = os.path.join(placespath, f'processed_{imsize}/data.h5py')
 
-unbiased_places = [
-                 'k/kasbah',
-                 'l/lighthouse',
-                 'p/pagoda',
-                 'r/rock_arch']
+# outfiles
+if not os.path.exists(outpath):
+    os.makedirs(outpath)
+uniff = os.path.join(outpath, f'uniform_data_{imsize}.npy')
+mashf = os.path.join(outpath, f'mashed_data_{imsize}.npy')
+unifyf = os.path.join(outpath, f'uniform_labels_{imsize}.npy')
+mashyf = os.path.join(outpath, f'mashed_labels_{imsize}.npy')
 
-validation_unbiased_places = [
-        'o/oast_house',
-        'o/orchard',
-        'v/viaduct']
+# load the data
+cocotrain = h5py.File(cocotrainf, 'r')
+cocoval = h5py.File(cocovalf, 'r')
+places = h5py.File(placesf, 'r')
 
-test_unbiased_places = [
-                 'w/water_tower',
-                 'w/waterfall',
-                 'z/zen_garden']
+# setup
+s_train = cocotrain['resized_images'].shape
+n_train = s_train[0]
+s_val = cocoval['resized_images'].shape
+n_val = s_val[0]
+places_uniform = places['resized_place'][:][:,0:n_train//10,:,:]
+places_uniform = places_uniform.reshape((-1,s_train[1],s_train[2],s_train[3]))
+places_mashed = places['resized_place'][:][:,n_train//10:,:,:]
+places_mashed = places_mashed.reshape((-1,s_val[1],s_val[2],s_val[3]))
 
+# first create and save the uniform data that have the factors fixed
+train_mask = cocotrain['resized_mask'][:] 
+X_uniform = train_mask*cocotrain['resized_images'][:] + (1-train_mask)*places_uniform
+y_uniform = cocotrain['y'][:]
+y_places = np.concatenate([places['y'][1000*c:1000*(c+1)-500] for c in range(10)])
+y_uniform = np.concatenate((y_uniform.reshape(-1,1),y_places.reshape(-1,1)),1)
+np.save(uniff, X_uniform)
+np.save(unifyf, y_uniform)
 
-biased_places = biased_places + unbiased_places + validation_unbiased_places + test_unbiased_places
-NUM_CLASSES = len(biased_places)
-
-
-print('----- Bias places ------')
-print(biased_places)
-
-dataset_name = 'cocoplaces'
-h5pyfname = os.path.join(output_dir, dataset_name)
-print('---------------- FNAME --------------')
-print(h5pyfname)
-if not os.path.exists(h5pyfname):
-    os.makedirs(h5pyfname)
-######################################################################################
-biased_place_fnames = {}
-for i, target_place in enumerate(biased_places):
-    L = [f'{target_place}/{filename}' for filename in os.listdir(os.path.join(places_dir, target_place)) if filename.endswith('.jpg')]
-    random.shuffle(L)
-    biased_place_fnames[i] = L
-
-
-tr_i = 1000 * NUM_CLASSES
-train_fname = os.path.join(h5pyfname,'places.h5py')
-if os.path.exists(train_fname): subprocess.call(['rm', train_fname])
-train_file = h5py.File(train_fname, mode='w')
-train_file.create_dataset('resized_place', (NUM_CLASSES,1000,3,64,64), dtype=np.dtype('float32'))
-
-tr_s, val_s, te_s = 0, 0, 0
-
-for c in range(NUM_CLASSES):
-
-    tr_si = 0
-    print('Class {} (train) : '.format(c), biased_places[c], end=' ')
-    while tr_si < 1000:
-        place_path = biased_place_fnames[c][tr_si]
-        place_img = np.asarray(Image.open(os.path.join(places_dir, place_path)).convert('RGB'))
-        # that's the one:
-        resized_place = resize(place_img, (64, 64))
-        train_file['resized_place'][c, tr_si, ...] = np.transpose(resized_place, (2,0,1))
-        tr_si += 1
-        if tr_si % 100 == 0: print('>'.format(c), end='')
-        ########################################
-    print('')
-
-train_file.close()
-
+# now  do the same for the mashed data - but mix the factors
+inds = np.array(random.sample(range(n_val), n_val))
+val_mask = cocoval['resized_mask'][:][inds]
+X_mashed = val_mask*cocoval['resized_images'][:][inds] + (1-val_mask)*places_mashed
+y_mashed = cocoval['y'][:][inds]
+y_places = np.concatenate([places['y'][1000*c+500:1000*(c+1)] for c in range(10)])
+y_mashed = np.concatenate((y_mashed.reshape(-1,1),y_places.reshape(-1,1)),1)
+np.save(mashf, X_mashed)
+np.save(mashyf, y_mashed)
